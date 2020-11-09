b3e1e3e4562ab6501278e293c29cc1e84ef02d0c
Nicola Milella <nicombk@gmail.com>
2020-11-09 17:53:21 +0100

    agregados valores old a las inputs, cambiada posicion boton del anuncio y fix ui

--- resources/views/announcement/new.blade.php
+++ resources/views/announcement/new.blade.php
@@ -22,17 +22,17 @@
                     <div class="form-group">
                         <label for="announcementeName">Titulo</label>
                         <input type="text" class="form-control" id="announcementeName" aria-describedby="emailHelp"
-                            name="title">
+                    name="title" value="{{old("title")}}">
                         @error('title')
-                        <small id="emailHelp" class="form-text" style="color:red;">{{ $message }}</small></div>
+                        <small id="emailHelp" class="form-text" style="color:red;">{{ $message }}</small>
                         @enderror
                     </div>
                     <div class="form-group">
                         <label for="announcementeBody">Anuncio</label>
                         <textarea class="form-control" name="body" id="announcementeBody" cols="30"
-                            rows="10"></textarea>
+                            rows="10">{{old("body")}}</textarea>
                         @error('body')
-                            <small id="emailHelp" class="form-text" style="color:red;">{{ $message }}</small></div>
+                            <small id="emailHelp" class="form-text" style="color:red;">{{ $message }}</small>
                         @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
